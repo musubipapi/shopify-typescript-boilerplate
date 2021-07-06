1c2e1f829150db588e4c46f4e714b0a7b2d6e6f1
Andy Gao <supagao7@gmail.com>
2021-07-05 20:47:02 -0400

    fix up typings

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+export {};
+declare global {
+  var API_KEY: string;
+}
+
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-import App from 'next/app';
+import App, { AppInitialProps } from 'next/app';
 import { AppProvider } from '@shopify/polaris';
 import { Provider, useAppBridge } from '@shopify/app-bridge-react';
 import { authenticatedFetch } from '@shopify/app-bridge-utils';
@@ -9,65 +9,62 @@ import '@shopify/polaris/dist/styles.css';
 import translations from '@shopify/polaris/locales/en.json';
 
 function userLoggedInFetch(app) {
-    const fetchFunction = authenticatedFetch(app);
+  const fetchFunction = authenticatedFetch(app);
 
-    return async (uri, options) => {
-        const response = await fetchFunction(uri, options);
+  return async (uri, options) => {
+    const response = await fetchFunction(uri, options);
 
-        if (response.headers.get('X-Shopify-API-Request-Failure-Reauthorize') === '1') {
-            const authUrlHeader = response.headers.get('X-Shopify-API-Request-Failure-Reauthorize-Url');
+    if (response.headers.get('X-Shopify-API-Request-Failure-Reauthorize') === '1') {
+      const authUrlHeader = response.headers.get('X-Shopify-API-Request-Failure-Reauthorize-Url');
 
-            const redirect = Redirect.create(app);
-            redirect.dispatch(Redirect.Action.APP, authUrlHeader || '/auth');
-            return null;
-        }
-
-        return response;
-    };
-}
-
-function MyProvider(props) {
-    const app = useAppBridge();
-
-    const client = new ApolloClient({
-        fetch: userLoggedInFetch(app),
-        fetchOptions: {
-            credentials: 'include',
-        },
-    });
-
-    const Component = props.Component;
-
-    return (
-        <ApolloProvider client={client}>
-            <Component {...props} />
-        </ApolloProvider>
-    );
-}
-
-class MyApp extends App {
-    render() {
-        const { Component, pageProps, host } = this.props;
-        return (
-            <AppProvider i18n={translations}>
-                <Provider
-                    config={{
-                        apiKey: API_KEY,
-                        host: host,
-                        forceRedirect: true,
-                    }}
-                >
-                    <MyProvider Component={Component} {...pageProps} />
-                </Provider>
-            </AppProvider>
-        );
+      const redirect = Redirect.create(app);
+      redirect.dispatch(Redirect.Action.APP, authUrlHeader || '/auth');
+      return null;
     }
+
+    return response;
+  };
 }
-//@ts-ignore
+
+const MyProvider = (props) => {
+  const app = useAppBridge();
+
+  const client = new ApolloClient({
+    fetch: userLoggedInFetch(app),
+    fetchOptions: {
+      credentials: 'include',
+    },
+  });
+
+  const Component = props.Component;
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...props} />
+    </ApolloProvider>
+  );
+};
+
+const MyApp = ({ Component, pageProps, host }) => {
+  return (
+    <AppProvider i18n={translations}>
+      <Provider
+        config={{
+          apiKey: API_KEY,
+          host: host,
+          forceRedirect: true,
+        }}
+      >
+        <MyProvider Component={Component} {...pageProps} />
+      </Provider>
+    </AppProvider>
+  );
+};
+
 MyApp.getInitialProps = async ({ ctx }) => {
-    return {
-        host: ctx.query.host,
-    };
+  return {
+    host: ctx.query.host,
+  };
 };
 
 export default MyApp;
